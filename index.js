ed12cac437f13599cda013da1dccba9d49264c36
Lotlin <lotlin22@gmail.com>
2023-04-17 22:41:09 +0200

    lesson_1

---
+++ index.js
@@ -0,0 +1,10 @@
+'use srtict';
+const goodName = "checkered 12";
+const goodAmount = 10;
+const goodCategory = "notebook";
+const goodPrice = 25;
+const goodTotalPrice = goodAmount * goodPrice;
+
+
+console.log ('Наименование товара: ' + goodName + '.');
+console.log ('Общая стоимость товара: ' + goodTotalPrice + ' рублей.');
