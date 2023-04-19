ab73201f78abbb19616793e27840f7fb53d7c2ea
Lotlin <lotlin22@gmail.com>
2023-04-19 17:00:01 +0200

    lesson-2 after remarks

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 'use srtict';
 
-let goodName = 'checkered 12';
-let goodAmount = 10;
-let goodCategory = 'notebook';
-let goodPrice = 25;
-let goodTotalPrice = goodAmount * goodPrice;
-
-console.log (`Наименование товара: ${goodName}.`);
-console.log (`Общая стоимость товара: ${goodTotalPrice} рублей.`);
-
-goodName = prompt('Введите наименование товара');
-goodAmount = prompt('Введите количество товара');
-goodCategory = prompt('Введите категорию товара');
-goodPrice = prompt('Введите цену товара');
+const goodName = prompt('Введите наименование товара', 'checkered 12');
+const goodAmount = + prompt('Введите количество товара', 10);
+const goodCategory = prompt('Введите категорию товара', 'notebook');
+const goodPrice = + prompt('Введите цену товара', 25);
 
 console.log(typeof(goodName));
 console.log(typeof(goodAmount));
 console.log(typeof(goodCategory));
 console.log(typeof(goodPrice));
 
-goodAmount = + goodAmount;
-goodPrice = + goodPrice;
 goodTotalPrice = goodAmount * goodPrice;
 
 console.log(`На складе ${goodAmount} единицы товара ${goodName} на сумму ${goodTotalPrice} деревянных`);
