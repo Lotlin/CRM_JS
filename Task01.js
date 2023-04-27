cbad35cf3ffdde3d77ca02343275d696af066542
Lotlin <lotlin22@gmail.com>
2023-04-27 15:02:55 +0200

    lesson_6_hard

--- Task01.js
+++ Task01.js
@@ -1,18 +1,39 @@
 'use strict';
 
-const allSrudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
-const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
+const allCashbox = [
+  [12, 4500], 
+  [7, 3210], 
+  [4, 650], 
+  [3, 1250], 
+  [9, 7830], 
+  [1, 990], 
+  [6, 13900], 
+  [1, 370]
+];
 
-const filter = (arrayAllSrudents, arrayFailedStudents) => {
-  const arrayPassedExam = []
-  
-  for (const elem of arrayAllSrudents) {
-    if (!arrayFailedStudents.includes(elem)) {
-      arrayPassedExam.push(elem);
-    }
-  }
+const  getAverageOneGoodPrice = arr => {
+  const numOfCheks = arr[0];
+  const totalSumOfCheck = arr[1];
+  const averagePriceOneGoodPrice = Math.round((totalSumOfCheck / numOfCheks) * 100) / 100; 
 
-  return arrayPassedExam;
+  return averagePriceOneGoodPrice;
 }
 
-console.log(`Студенты, сдавшие экзамен: ${filter(allSrudents, failedStudents)}.`);
+
+const getAveragePriceGoods = arr => {
+
+  const arrAverageCheckPrice = arr.map(getAverageOneGoodPrice);
+  let totalSum = 0;
+  let numOfElem = 0;
+
+  for (const elem of arrAverageCheckPrice) {
+    totalSum += elem;
+    numOfElem += 1;
+  }
+
+  const averagePriceGoods = Math.round((totalSum / numOfElem) * 100) / 100; 
+
+  return averagePriceGoods;
+}
+
+console.log(`Средняя стоимость одного товара в магазине: ${getAveragePriceGoods(allCashbox)} руб.`);
