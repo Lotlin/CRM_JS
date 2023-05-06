612f1aa70403eb5e8ebbdcb9685a0ab7720d9761
Lotlin <lotlin22@gmail.com>
2023-05-06 17:38:14 +0200

    l_7_t_1

--- Task01.js
+++ Task01.js
@@ -1,39 +1,16 @@
 'use strict';
 
-const allCashbox = [
-  [12, 4500], 
-  [7, 3210], 
-  [4, 650], 
-  [3, 1250], 
-  [9, 7830], 
-  [1, 990], 
-  [6, 13900], 
-  [1, 370]
-];
+const arrGenerator = num => {
+  
+  const arr = [];
+  const min = 1;
+  const max = 100;
 
-const  getAverageOneGoodPrice = arr => {
-  const numOfCheks = arr[0];
-  const totalSumOfCheck = arr[1];
-  const averagePriceOneGoodPrice = Math.round((totalSumOfCheck / numOfCheks) * 100) / 100; 
-
-  return averagePriceOneGoodPrice;
-}
-
-
-const getAveragePriceGoods = arr => {
-
-  const arrAverageCheckPrice = arr.map(getAverageOneGoodPrice);
-  let totalSum = 0;
-  let numOfElem = 0;
-
-  for (const elem of arrAverageCheckPrice) {
-    totalSum += elem;
-    numOfElem += 1;
+  for (let i = 0; i < num; i++) {
+    arr.push(Math.round(Math.random() * (max - min)) + min);
   }
 
-  const averagePriceGoods = Math.round((totalSum / numOfElem) * 100) / 100; 
-
-  return averagePriceGoods;
+  return arr;
 }
 
-console.log(`Средняя стоимость одного товара в магазине: ${getAveragePriceGoods(allCashbox)} руб.`);
+console.log(`Массив чисел в диапазоне 1 - 100: ${arrGenerator(30)}.`);
